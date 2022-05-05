061c74ecc18faafe6187531e5f42c46be9a63905
kavishka199808 <it20301590@my.sliit.lk>
2022-05-05 07:08:22 -0700

    crud operation

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username,password;
+    Button login,register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.etUserNameMain);
+        password = findViewById(R.id.etPasswordMain);
+        login = findViewById(R.id.btnlogin);
+        register = findViewById(R.id.btnregister);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),ProfileManagement.class);
+                startActivity(i);
+            }
+        });
+    }
+}
